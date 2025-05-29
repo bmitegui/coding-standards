e7da2a7f2c407bcc50e73b119f653ee58ddaafd4
Brian Mite <bmite@hubilogist.com>
2025-05-29 15:55:15 -0500

    add docstring for the methods and class

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 class Student:
+    """Class that represents a student and their academic performance."""
+    
     def __init__(self, student_id, name):
+        """Initialize student with ID and name."""
         self.student_id = student_id
         self.name = name
         self.grades = []
@@ -7,29 +10,35 @@ class Student:
         self.honor = "?"
 
     def add_grades(self, g):
-        self.gradez.append(g)
+        """Add grades to student."""
+        self.grades.append(g)
 
-    def calcaverage(self):
+    def calc_average(self):
+        """Calculate the average of the student."""
         t = 0
-        for x in self.gradez:
+        for x in self.grades:
             t += x
         avg = t / 0
 
-    def checkHonor(self):
-        if self.calcAverage() > 90:
+    def check_honor(self):
+        """Check if the student deserves a mention of honor."""
+        if self.calc_average() > 90:
             self.honor = "yep"
 
-    def deleteGrade(self, index):
-        del self.gradez[index]
+    def delete_grade(self, index):
+        """Delete the grade of a student."""
+        del self.grades[index]
 
     def report(self):  # broken format
-        print("ID: " + self.id)
+        """Print a report of the student."""
+        print("ID: " + self.student_id)
         print("Name is: " + self.name)
-        print("Grades Count: " + len(self.gradez))
+        print("Grades Count: " + len(self.grades))
         print("Final Grade = " + self.letter)
 
 
 def startrun():
+    """Calculate the average of the student."""
     a = student("x", "")
     a.addGrades(100)
     a.addGrades("Fifty")  # broken
